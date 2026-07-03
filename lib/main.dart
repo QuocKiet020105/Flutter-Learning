75e6e2123e9f88a46f7a8d5cba90946c57212758
QuocKiet020105 <kullkiet725@gmail.com>
2026-07-03 08:43:34 +0700

    Buoi 2

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+
+
+void main() {
+  // var firstName = "Quoc";
+  // var lastName = "Kiet";
+  // print("Full name is $firstName $lastName");
+
+
+  // int num1 = 10;
+  // int num2 = 3;
+  //
+  // //toan tu
+  // int sum = num1 + num2;
+  // int diff = num1 - num2;
+  // int mull = num1 * num2;
+  // double div = num1 / num2;
+  // int quotient = num1 ~/ num2;
+  // int remainder = num1 % num2;
+
+  //khai bao pi
+  //  const pi = 3.14;
+  //  print("So pi la:  $pi");
+  //
+  //
+  // //display
+  // print(" The sum is $sum");
+  // print(" The diff is  $diff");
+  // print(" The mull is $mull");
+  // print(" The double is $div");
+  // print("The Quotient is $quotient");
+  // print("the Remainder is $remainder");
+
+
+  //Me
+  // String name = "Kiet";
+  // String address= "VN";
+  // num age = 20;
+  // num height = 1.73;
+  // bool isMarride = false;
+  //
+  // print("Name is $name");
+  // print("Address is $address");
+  // print("Age: $age");
+  // print("Height: $height");
+  // print("Ismarried: $isMarride");
+  //
+  //
+  // int num1 = 100;
+  // double num2 = 130.2;
+  // num num3 = 50;
+  // num num4 = 50.4;
+
+  //sum
+  // num sum = num1 + num2 + num3 + num4;
+  //
+  // print("Num1: $num1");
+  // print("Num2: $num2");
+  // print("Num3: $num3");
+  // print("Num4: $num4");
+  // print("Sum: $sum");
+
+  // double price = 1130.2232323233233;
+  // print(price.toStringAsFixed(2));
+  //
+  // String multiLineText = '''
+  // This is Mutil Line Text
+  // with 3 single quote
+  // I`m also writing here
+  // ''';
+
+  // print("Mutil Line Text $multiLineText");
+  //
+  // //xuong dong co the dung /n hoac /t
+  //
+  // num price1 = 10;
+  // String withouutRawString = "The value of price is \t $price1";
+  // String withRawString = r"The value of price is \t $price1";
+
+  // print("WithoutRawString: $withouutRawString");
+  // print("WithRawString: $withRawString");
+
+  String strvalue = "1";
+  print("Type of strvalue is ${strvalue.runtimeType}");
+
+  int intvalue = int.parse(strvalue);
+  print("Value of intvalue is $intvalue");
+
+  print("Type of intvalue is ${intvalue.runtimeType}");
+
+}
+
+
